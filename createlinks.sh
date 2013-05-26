a3be1388caf87ad36f9f87365633cee5223deabd
Marcus <m@m>
2013-05-26 16:25:31 +0200

    Updated script to make links

--- createlinks.sh
+++ createlinks.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 ln -s dotfiles/.bashrc .bashrc
+ln -s dotfiles/.gitconfig .gitconfig
+ln -s dotfiles/.gitmodules .gitmodules
+ln -s dotfiles/.gvimrc .gvimrc
+ln -s dotfiles/.vim .vim
+ln -s dotfiles/.vimrc .vimrc
