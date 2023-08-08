7e8be01af6f55867923003ef27948029d4075540
Marcus <m@m>
2023-08-08 08:03:43 +0200

    Updated mini.surround mappings

--- .config/nvim/lua/core/plugins/mini.lua
+++ .config/nvim/lua/core/plugins/mini.lua
@@ -4,10 +4,16 @@ return {
         require('mini.bracketed').setup()
         require('mini.surround').setup {
             mappings = {
-                add = 'ys', -- Add surrounding in Normal and Visual modes
-                delete = 'ds', -- Delete surrounding
-                find = 'fs', -- Find surrounding (to the right)
-                replace = 'rs', -- Replace surrounding
+                add = 'za', -- Add surrounding in Normal and Visual modes
+                delete = 'zd', -- Delete surrounding
+                find = 'zf', -- Find surrounding (to the right
+                find_left = 'zF', -- Find surrounding (to the left)
+                highlight = 'zh', -- Highlight surrounding
+                replace = 'zr', -- Replace surrounding
+                update_n_lines = 'zn', -- Update `n_lines`
+
+                suffix_last = 'l', -- Suffix to search with "prev" method
+                suffix_next = 'n', -- Suffix to search with "next" method
             }
         }
     end,
